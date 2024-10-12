6791661818ddc1864df46ae1caa556e22fa3dc56
Dukepq <maikelvdj@live.be>
2024-10-12 03:04:30 +0200

    fix: static export

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  images: {
+    // for the static export to work
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
